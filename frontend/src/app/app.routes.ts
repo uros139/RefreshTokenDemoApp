57c7b6a4366d1bf907526daa216486bf23608a85
Uros Totovic <urostotovic@gmail.com>
2024-01-23 18:04:16 +0100

    Logn page added, several changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { StudentsListComponent } from './features/students-list/students-list.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     { path: 'students-list', component: StudentsListComponent }
